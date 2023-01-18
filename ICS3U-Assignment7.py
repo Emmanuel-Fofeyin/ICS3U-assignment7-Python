ecf350a952b5574acccf171ed64c295ea55e03a7
Emmanuel Fofeyin Ngwamunwi <emmanuel.fofeyin-ngwamunwi@mths.ca>
2023-01-18 03:42:31 +0000

    code added

---
+++ ICS3U-Assignment7.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# Created by: Emmanuel Fofeyin
+# Created on: Dec 2023
+# This program combines two lists by alternatingly taking elements.
+
+
+def concatenates_lists(list1, list2):
+    for a_single_item in list2:
+        list1.append(a_single_item)
+    concatenated_list = list1
+    return concatenated_list
+
+
+def main():
+    # this function gets two lists
+
+    first_list = []
+    second_list = []
+    combined_lists = []
+
+    # input
+    print("Please enter 5 items to place in the list: ")
+    for counter in range(1, 6):
+        item = input("Item {0}: ".format(counter))
+        first_list.append(item)
+
+    print("Please enter 5 items to place in the second list: ")
+    for counter in range(1, 6):
+        item = input("Item {0}: ".format(counter))
+        second_list.append(item)
+
+    # process and output
+    # calls function
+    combined_lists = concatenates_lists(first_list, second_list)
+    print("")
+    print("Here is your combined list: ")
+    print("{0}".format(combined_lists))
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
+    
